8dcbaf3413f410c69734be9bbdcd48eedddd8910
yibaebi <elliot.yibaebi@yahoo.com>
2022-07-13 13:14:09 +0100

    add app assets

---
+++ src/assets/images/index.js
@@ -0,0 +1,5 @@
+import programImg from './program_img.png'
+import speakerMan from './speaker_man.png'
+import speakerWoman from './speaker_wman.png'
+
+export { programImg, speakerMan, speakerWoman }
---
+++ src/assets/svgs/index.js
@@ -0,0 +1,8 @@
+import { ReactComponent as AltSchool } from './altSchool.svg'
+import { ReactComponent as CowryWise } from './cowrywise.svg'
+import { ReactComponent as GreenTick } from './greenTick.svg'
+import { ReactComponent as Kuda } from './kuda.svg'
+import { ReactComponent as RightArrow } from './rightArrow.svg'
+import { ReactComponent as SlantedArrow } from './slantedArrow.svg'
+
+export { GreenTick, AltSchool, Kuda, CowryWise, SlantedArrow, RightArrow }
