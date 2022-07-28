8b817f8b8a9890eea89edc595053b31992284399
yibaebi <elliot.yibaebi@yahoo.com>
2022-07-28 23:24:51 +0100

    update round button component

--- src/components/RoundButton/index.js
+++ src/components/RoundButton/index.js
@@ -1,5 +1,4 @@
 import React, { useMemo } from 'react'
-import { RightArrow, SlantedArrow } from '../../assets'
 import { useWindowSize } from '../../hooks'
 import styles from './styes.module.css'
 
@@ -71,8 +70,32 @@ const RoundButton = ({ position = '', size = 'small', color = 'black', onClick =
           )}
         </React.Fragment>
       ) : (
-        <button className={`${styles.button} ${styles[position]} ${styles[size]}`} onClick={() => onClick()}>
-          <React.Fragment>{size === 'small' ? <SlantedArrow /> : <RightArrow />}</React.Fragment>
+        <button id="circle" className={`white ${styles[position]}`} onClick={() => onClick()}>
+          <svg
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            x="0px"
+            y="0px"
+            width="300px"
+            height="300px"
+            viewBox="0 0 300 300"
+            enableBackground="new 0 0 300 300"
+            xmlSpace="preserve"
+          >
+            <defs>
+              <path id="circlePath" d=" M 150, 150 m -60, 0 a 60,60 0 0,1 120,0 a 60,60 0 0,1 -120,0 " />
+            </defs>
+            <circle cx="168" cy="168" r="84" fill="none" />
+            <g>
+              <use xlinkHref="#circlePath" fill="none" />
+              <text fill="#fff" width={168} height={168}>
+                <textPath xlinkHref="#circlePath" textLength="370">
+                  Register to attend for free&nbsp;
+                </textPath>
+              </text>
+            </g>
+          </svg>
         </button>
       )}
     </React.Fragment>
